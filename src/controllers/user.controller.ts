305d0d844a7d8c992ae8d76c99455d8d230f0a39
leobusta <leobusta@gmail.com>
2022-10-25 20:42:00 -0500

    oct 25 clase

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { createUser, findUserByEmail } from "../services/user.service";
 import debug from 'debug';
 import bcrypt from 'bcrypt';
+import jwt, {Secret} from 'jsonwebtoken';
 
 const debugLog: debug.IDebugger = debug('app')
 
@@ -28,7 +29,11 @@ export async  function login(req: Request, res: Response) {
     try{
         const user = await findUserByEmail(req.body.email); 
         if ( user !== null && await bcrypt.compare(req.body.password,user.password)){
-            
+            const token = jwt.sign({user_id: user._id, email: user.email}, process.env.TOKENSECRET as Secret, {expiresIn: "2h"})
+            user.token = token
+            user.password = ""
+
+            res.status(200).send(user)
         }
 
         return res.status(401).send("user or password not valid");
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,6 +8,7 @@ export interface UserInput {
 }
 
 export interface UserDocument  extends UserInput, mongoose.Document {
+    token: string;
     createAt: Date;
     updateAt: Date;
 }
@@ -15,7 +16,7 @@ export interface UserDocument  extends UserInput, mongoose.Document {
 const userSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     name: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
 }, {
     timestamps: true,
 }
