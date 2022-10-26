4f57be8913c3bdd9191ce91283be9a0332f1e02d
leobusta <leobusta@gmail.com>
2022-10-25 21:02:58 -0500

    controller function to class

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,44 +1,55 @@
 import { Request, Response } from "express";
 import { createUser, findUserByEmail } from "../services/user.service";
-import debug from 'debug';
-import bcrypt from 'bcrypt';
-import jwt, {Secret} from 'jsonwebtoken';
+import debug from "debug";
+import bcrypt from "bcrypt";
+import jwt, { Secret } from "jsonwebtoken";
+import { UserDocument } from "../models/user.model";
 
-const debugLog: debug.IDebugger = debug('app')
+const debugLog: debug.IDebugger = debug("app");
 
+class UserController {
+  async createUserHandler(req: Request, res: Response) {
+    try {
+      const userExist = await findUserByEmail(req.body.email);
+      if (userExist !== null) {
+        return res.status(409).send("user exist");
+      }
 
-export async function createUserHandler( req: Request, res: Response){
-    try{
-        const userExist = await findUserByEmail(req.body.email); 
-        if ( userExist !== null){
-            return res.status(409).send("user exist");
-        }
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+      const user = await createUser(req.body);
+      return res.send(user);
+    } catch (e: any) {
+      debugLog(e);
 
-        req.body.password  = await bcrypt.hash(req.body.password, 10);
-        const user = await createUser(req.body)
-        return res.send(user)
-    }catch(e: any){
-        debugLog(e);
-
-        return res.status(409).send(e.message);
+      return res.status(409).send(e.message);
     }
+  }
 
+  async login(req: Request, res: Response) {
+    try {
+      const user: UserDocument | null = await findUserByEmail(req.body.email);
+      if (
+        user !== null &&
+        (await bcrypt.compare(req.body.password, user.password))
+      ) {
+        const token = await jwt.sign(
+          { user_id: user._id, email: user.email },
+          process.env.TOKENSECRET as Secret,
+          { expiresIn: "2h" }
+        );
+
+        return res
+          .status(200)
+          .send({ email: user.email, name: user.name, token });
+      }
+
+      return res.status(401).send("Invalid credentials");
+    } catch (e: any) {
+      debugLog(e);
+
+      return res.status(409).send(e.message);
+    }
+  }
 }
 
-export async  function login(req: Request, res: Response) {
-    try{
-        const user = await findUserByEmail(req.body.email); 
-        if ( user !== null && await bcrypt.compare(req.body.password,user.password)){
-            const token = await jwt.sign({user_id: user._id, email: user.email}, process.env.TOKENSECRET as Secret, {expiresIn: "2h"})
-            
-            return res.status(200).send({email: user.email, name: user.name, token})
-        }
-
-        return res.status(401).send("Invalid credentials");
-    }catch(e: any){
-        debugLog(e);
-
-        return res.status(409).send(e.message);
-    }
-
-}
+export default new UserController();
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,9 @@
 import { Express, Request, Response } from "express";
-import { createUserHandler, login }  from "../controllers/user.controller";
+import UserController  from "../controllers/user.controller";
 
 function  routes(app: Express) {
-    app.post("/api/users", createUserHandler)
-    app.post("/api/sessions", login)
+    app.post("/api/users", UserController.createUserHandler)
+    app.post("/api/sessions", UserController.login)
 }
 
 export default routes;
